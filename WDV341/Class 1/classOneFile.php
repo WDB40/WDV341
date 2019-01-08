5c8dc393a9453411e397b0dd33dc73c9e42178fd
WDB40 <wbrown1640@gmail.com>
2019-01-07 20:29:53 -0600

    End of Class 01/07/19

---
+++ WDV341/Class 1/classOneFile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Class One File</title>
+</head>
+<body>
+
+    <h1>Hello Class PHP Processor</h1>
+
+    <?php
+        echo "<h2>This is the PHP</h2>";
+    ?>
+
+    <p>This is my name <?php echo "Wes"; ?></p>
+
+</body>
+</html>
