24cf2607f319c1ca437f620fe4ff59b24638e146
WDB40 <wbrown1640@gmail.com>
2019-01-09 19:48:32 -0600

    Completing step 4 of GitHub assignment.

---
+++ WDV341/Class 1/testGit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TestGit</title>
+</head>
+<body>
+
+<?php
+
+    echo "<h1>Wes Brown</h1>";
+
+?>
+
+</body>
+</html>
---
+++ WDV341/General/wdv341.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WDV341</title>
+</head>
+<body>
+
+<h1>Homepage for WDV341 - Intro to PHP</h1>
+<h3>Wes Brown</h3>
+
+<h2>Assignments</h2>
+<ul>
+    <li></li>
+</ul>
+
+</body>
+</html>
